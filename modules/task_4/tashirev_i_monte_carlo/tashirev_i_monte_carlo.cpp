6ce7fb86942f0d107e0a7baee5646bab6fabea10
TashirevIvan <tashirev.ivan@yandex.ru>
2021-06-04 20:19:18 +0300

    v5

--- modules/task_4/tashirev_i_monte_carlo/tashirev_i_monte_carlo.cpp
+++ modules/task_4/tashirev_i_monte_carlo/tashirev_i_monte_carlo.cpp
@@ -46,16 +46,16 @@ double stdMonteCarlo(double(*f)(const std::vector<double>&),
     double res = 0.0;
     auto mult = a.size();
     std::vector<std::uniform_real_distribution<double>> r(mult);
-    for (int i = 0; i < mult; i++)
+    for (size_t i = 0; i < mult; i++)
         r[i] = std::uniform_real_distribution<double>(a[i], b[i]);
 
     auto num_threads = std::thread::hardware_concurrency();
     std::vector<std::thread> threads;
     std::vector<double> local_res(num_threads);
     auto start = 0;
-    auto k = steps % num_threads;
+    int k = steps % num_threads;
 
-    for (auto i = 0; i < num_threads; ++i) {
+    for (size_t i = 0; i < num_threads; ++i) {
         my_range range;
         if (k > i) {
             range.start = start;
@@ -71,8 +71,8 @@ double stdMonteCarlo(double(*f)(const std::vector<double>&),
             gen.seed(static_cast<unsigned int>(time(0)));
             std::vector<double> r1(mult);
             local_res[i] = 0;
-            for (auto k = range.start; k < range.end; ++k) {
-                for (auto j = 0; j < mult; ++j)
+            for (long unsigned int k = range.start; k < range.end; ++k) {
+                for (size_t j = 0; j < mult; ++j)
                     r1[j] = r[j](gen);
                 local_res[i] += f(r1);
             }
@@ -83,12 +83,12 @@ double stdMonteCarlo(double(*f)(const std::vector<double>&),
         thread.join();
     }
 
-    for (auto i = 0; i < num_threads; ++i) {
+    for (size_t i = 0; i < num_threads; ++i) {
         res += local_res[i];
     }
 
     double S = 1;
-    for (int i = 0; i < mult; i++)
+    for (size_t i = 0; i < mult; i++)
         S *= (b[i] - a[i]);
     res *= S / steps;
 
